3a87bbb8f4419f29570e8ce9d79630b1b3b8a11a
Cheshiya <puppetbelladona@gmail.com>
2026-01-27 11:32:24 -0500

    Add initial project updates

---
+++ index.py
@@ -0,0 +1,12 @@
+print("hello Groupe2")
+
+
+lesMembres='Clement,Elsa,Junior,Elisé,Josephak'
+print(lesMembres)
+
+print("deuxieme modification")
+
+print("troisieme modification")
+
+
+print('quatrieme mise a jour')
